5aa6113b69d583c2d00c6d8acc68768db8668a74
mbexel <md_dicroce@hotmail.com>
2021-04-14 16:06:11 -0300

    commit al servidor Github

--- src/App.js
+++ src/App.js
@@ -1,90 +1,177 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Countries from './Countries'
-
+import dbConections from './Services/dbConection'
 const App = () => {
-  return (
-    <div>
-      <Countries />
-    </div>
-  )
-  /* const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([])
+  const [mensaje, setMensaje] = useState("");
   useEffect(() => {
-  axios.get('https://jsonplaceholder.typicode.com/users')
-    .then(response => {
-      setPersons(response.data)
-    })
-  },[])
+    dbConections.getAll()
+      .then(response => {
+        setPersons([...response])
+      })
+  }, [])
+  const [stilo, setStilo] = useState(success);
   const [newName, setNewName] = useState('')
   const [filter, newFilter] = useState('')
-  const changeHandler = (status) =>{
+  const changeHandler = (status) => {
     let value = status["target"]["value"];
     let name = status.target.name;
-    let aux = {...newName};
+    let aux = { ...newName };
     aux[name] = value;
-    setNewName({...aux});
-    
+    setNewName({ ...aux });
   }
-  const subHandler = (status) =>{
+  const changeNumber = () => {
+    const PersonToChange = {
+      "name" : newName.name,
+      "number" : newName.number
+    }
+    let id = persons.find((actual)=>newName.name == actual.name).id;
+    console.log(id);
+    dbConections.modify(id,PersonToChange)
+    .then(response => {
+      setPersons(persons.map((actual)=>{
+        if(actual.id !== id)
+        {
+          return actual;
+        }
+        else
+        {
+          return response;
+        }
+      }))
+    })
+    .catch(error => {
+      setStilo(fail);
+      setMensaje(`Information of ${PersonToChange.name} has been deleted`);
+      setTimeout(()=>{setMensaje("")
+      setStilo(success);
+    },2000);
+    }
+    )
+  }
+  const subHandler = (status) => {
     status.preventDefault();
-    
-    if(persons.some((value)=>value.name === newName.name))
-    {
-      alert(`${newName.name} is already added to the phonebook`)
-    }
-    else
-    {
-      setPersons(persons.concat(newName));
-    }
-    status.target[0].value = "";
-    status.target[1].value="";
-    
-  }
-  return (
-    <div>
-      <Filter newFilter = {newFilter}/>
-      <h2>Phonebook</h2>
-      <form onSubmit={subHandler}>
-        <div>
-          name: <input name="name" onChange={changeHandler} type="text"/>
-          number: <input name="number" onChange={changeHandler}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ShowNumbers numbers={persons} filter={filter}  />
-    </div>
-  ) */
+    if (persons.some((value) => value.name === newName.name)) 
+      {
+        if(window.confirm(`${newName.name} is already added to the phonebook,do you want to change the number?`))
+        changeNumber();
+
+      }
+    else {
+      const personToAdd = {
+        "name": newName.name,
+        "number": newName.number
+      }
+      
+      dbConections.create(personToAdd)
+        .then(response => {
+          setPersons([...persons,response])
+          setMensaje(`Added ${response.name}`);
+        })
+        setTimeout(()=>setMensaje(""),2000)
+      }
+  status.target[0].value = "";
+  status.target[1].value = "";
+
 }
-const Filter = ({newFilter}) => {
+
+return (
+  <div>
+    <Filter newFilter={newFilter} />
+    <Mensaje  mensaje={mensaje} stilo={stilo}/>
+    <h2>Phonebook</h2>
+    <form onSubmit={subHandler}>
+      <div>
+        name: <input name="name" onChange={changeHandler} type="text" />
+          number: <input name="number" onChange={changeHandler} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+    <h2>Numbers</h2>
+    <ShowNumbers numbers={persons} filter={filter} setPersons={setPersons} />
+  </div>
+)
+}
+const success = {
+  color: "green",
+  background: "lightgrey",
+  fontSize: "20px",
+  borderStyle: "solid",
+  borderRadius: "5px",
+  padding: "10px",
+  marginBottom: "10px"
+}
+const fail = {
+  color: "red",
+  background: "lightgrey",
+  fontSize: "20px",
+  borderStyle: "solid",
+  borderRadius: "5px",
+  padding: "10px",
+  marginBottom: "10px"
+}
+const Filter = ({ newFilter }) => {
   const filterHandler = (status) => {
     newFilter(status.target.value);
   }
-  return ( <form>
-    <input type="text" onChange={filterHandler}/>
+  return (<form>
+    <input type="text" onChange={filterHandler} />
 
   </form>
   )
 }
-const ShowNumbers = ({numbers,filter}) =>
+const Mensaje = ({mensaje,stilo}) => 
 {
-  return (
-    numbers.map((actual) =>{
-      if(actual.name.search(filter) !== -1)
-      {
-        return (
-          <div key={actual.name}>
-  
-            <p> {actual.name} <i> {actual.phone} </i> </p>
-  
-          </div>
-        )
-        
-      }
-
-    })
-  )
+  if(mensaje)
+  {
+    return (
+      <div style={stilo}>{mensaje}</div>
+    )
+  }
+  else
+  {
+    return (
+      <></>
+    )
+  }
 }
+const ShowNumbers = ({ numbers, filter, setPersons}) => {
+  const regEx = new RegExp(`${filter}`, "i");
+  if (numbers.length !== 0) {
+    return (
+      <div>
+        {numbers.filter((actual) => actual.name).filter((actual) => actual.name.match(regEx)).map((actual) => {
+          return (
+            <div key={actual.id}>
+              <p><b>Name: </b>{actual.name}<b> Number: </b>{actual.number}</p>
+              <DeleteButton id={actual.id} setPersons={setPersons} number ={numbers}/>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+  else {
+    return (<div></div>)
+  }
+}
+const DeleteButton = ({id,setPersons,number}) =>
+{
+  const deleteFunction = () =>
+  {
+    if(window.confirm(`Desea eliminar a ${number.find((actual)=>actual.id === id).name}`))
+    {
+      dbConections.deleteThis(id)
+      .then(() => setPersons(number.filter((actual)=>actual.id !== id))
+      )
+
+    }
+  }
+  return (
+    <button onClick={deleteFunction}>
+      Delete
+    </button>
+  )
+} 
 export default App;
